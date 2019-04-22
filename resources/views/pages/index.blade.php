31fde32b389b93b139bcc105c1845383926c71f9
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-04-22 15:54:37 +0600

    changes

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+    <div class="jumbotron text-center">
+        <h1>{{$title}}</h1>
+        <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+        <p><a class="" href=""></a></p>
+    </div>
 @endsection
