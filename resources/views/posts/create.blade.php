299fddfccedd98377c5c78a59e59d28e1bdee456
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-04-23 23:18:31 +0600

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,5 +7,9 @@
             {{Form::label('title', 'Title')}}
             {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
         </div>
+        <div class="form-group">
+            {{Form::label('body', 'Body')}}
+            {{Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Body Text'])}}
+        </div>
     {!! Form::close() !!}
 @endsection
