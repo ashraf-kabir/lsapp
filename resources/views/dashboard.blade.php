ab8d0395bbc334004af87df95b644874f8017b6d
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-04-24 07:49:10 +0600

    changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,7 +11,26 @@
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
                     <br><br>
                     <h3>Your Blog Posts</h3>
+                    @if (count($posts) > 0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach($posts as $post)
+                                <tr>
+                                    <th>{{$post->title}}</th>
+                                    <th><a href="/posts/{{$post->id}}/edit" class="btn btn-info" style="color: #fff;">Edit</a></th>
+                                    <th></th>
+                                </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You have no posts</p>
+                    @endif
                 </div>
+
             </div>
         </div>
     </div>
