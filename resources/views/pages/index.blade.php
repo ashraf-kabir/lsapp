042feb2e3ddcc918c9d5e343ef3c36191ccd51b3
Ashraf Kabir <s.m.ashraf.kabir@g.bracu.ac.bd>
2019-04-24 21:00:43 +0600

    Update index.blade.php

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="jumbotron text-center">
         <h1>{{$title}}</h1>
-        <p>This is the laravel application from the "Laravel From Scratch" Youtube series</p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida massa nisi, a dignissim est tristique vel. Aliquam bibendum consectetur quam ac egestas. Quisque volutpat tellus quis aliquet consectetur. Etiam cursus mattis purus a iaculis. Aliquam eget porta nulla, eget consequat ex. Morbi quis odio dui. Phasellus sollicitudin gravida augue sed rhoncus. Praesent tempor posuere sem, in dignissim ante facilisis nec. Morbi et porta est, sit amet cursus lectus. Nunc nisi arcu, facilisis sit amet dui ac, semper laoreet quam.
+        </p>
         <p>
             <a class="btn btn-primary btn-lg" href="/login" role="button">Login</a>
             <a class="btn btn-success btn-lg" href="/register" role="button">Register</a>
